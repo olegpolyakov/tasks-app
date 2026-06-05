6c4af13d1a4961ead1f48c06d2a8c209d221c0a6
Oleg Polyakov <olegpolyakov@outlook.com>
2026-06-05 19:59:27 +0800

    feat: wrap children in AuthProvider and export AUTH_URL from env

--- src/app/AppDataProvider.tsx
+++ src/app/AppDataProvider.tsx
@@ -2,15 +2,18 @@ import { AuthProvider } from '@/features/auth';
 import { SettingsProvider } from '@/features/settings';
 import { TagsProvider } from '@/features/tags';
 import { TasksProvider } from '@/features/tasks';
+import { AUTH_URL } from '@/shared/env';
 
 export default function AppDataProvider({ children }: {children: React.ReactNode}) {
     return (
-        <SettingsProvider>
-            <TasksProvider>
-                <TagsProvider>
-                    {children}
-                </TagsProvider>
-            </TasksProvider>
-        </SettingsProvider>
+        <AuthProvider apiUrl={AUTH_URL}>
+            <SettingsProvider>
+                <TasksProvider>
+                    <TagsProvider>
+                        {children}
+                    </TagsProvider>
+                </TasksProvider>
+            </SettingsProvider>
+        </AuthProvider>
     );
 }
